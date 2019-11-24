bb56fdd0a9a13557846a1b312bb86bbc3d990179
Nidhal Arouri <arrouurii@gmail.com>
2019-11-24 22:47:26 +0100

    [STEP1] 	- Add comments 	- Handle different errors on API fetch

--- src/components/nearObjectsChart.jsx
+++ src/components/nearObjectsChart.jsx
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import Chart from 'react-google-charts';
 import { from } from 'rxjs';
 import axios from 'axios';
+/* 
+    Nidhal AROURI 
+    24/11/2019
+    Botify/
+*/
+// ------------NEO Bar Chart--------------
 class NearObjectsChart extends Component {
+    // ----------State--------------
     state = { 
-        nearObjects: [],
-        visuData : []
+        nearObjects: [], // Contains NEO got from API
+        visuData : [] // Contains visualization data
     }
-    m = "estimated_diameter_m";
-    headers = ['NEO Name', 'min', 'max'];
+    m = "estimated_diameter_m"; // to minimize attribute length
+    headers = ['NEO Name', 'min', 'max']; // Chart headers
+    // ------------average(a, b)- Calculates a and b average-------------
     average(a, b) {
         return (a + b) / 2;
     }
+    // ------------Tronsform API data to visualization data structure-------------------
     mapNearObjectToVisu(nearObjects) {
         
         return nearObjects.map(
@@ -24,11 +33,14 @@ class NearObjectsChart extends Component {
             }
         );
     }
+    // -------------------------React Life-cycle hook / executed after the component has been mounted---------------------------
     componentDidMount() {
-        from(axios.get('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY'))
+        from(axios.get('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY')) // API call transformed to Observable
             .subscribe(
                 data => {
                     const nearObjects = data.data.near_earth_objects;
+                    
+                    // sorting fetched data [DESC, AVG-estimated_diameter.kilometers]
                     nearObjects.sort(
                         (elt1, elt2) =>  (
                             this.average(
@@ -41,7 +53,11 @@ class NearObjectsChart extends Component {
                             )
                         )
                     );
-                    this.setState({nearObjects: nearObjects});
+                    // change nearObjects state
+                    this.setState(
+                        {nearObjects: nearObjects}
+                    );
+                    // change visuData's state after updating nearObjects
                     this.setState(
                         {
                             visuData: [
@@ -52,7 +68,23 @@ class NearObjectsChart extends Component {
                     );
                 },
                 error => {
-                    return console.error(error);
+                    if (error.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        // The request was made but no response was received
+                        // `error.request` is an instance of XMLHttpRequest in the 
+                        // browser and an instance of
+                        // http.ClientRequest in node.js
+                        console.log(error.request);
+                    } else {
+                        // Something happened in setting up the request that triggered an Error
+                        console.log('Error', error.message);
+                    }
+                    console.log(error.config);
                 }
             );
     }
