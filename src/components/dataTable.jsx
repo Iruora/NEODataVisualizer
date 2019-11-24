94b3dae489cc063cbe9109f9ab0277888f8c76eb
Nidhal Arouri <arrouurii@gmail.com>
2019-11-25 00:43:43 +0100

    [ STEP3] 	- Add index to table rows

--- src/components/dataTable.jsx
+++ src/components/dataTable.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 class DataTable extends Component {
+    index = 0;
     mapToTable(array) {
         const data = array;
         data.shift();
         return data;
     }
     render() { 
-        console.log(this.props.visuData);
+        this.index = 0;
         return ( 
             <table className="table">
                 <thead className="thead-dark">
                     <tr>
+                        <th scope="col">#</th>
                         <th scope="col">Name</th>
                         <th scope="col">Max</th>
                         <th scope="col">Min</th>
@@ -19,11 +21,17 @@ class DataTable extends Component {
                 <tbody>
                     {
                         this.props.visuData.slice(1).map(
-                            e => <tr key={e[0]}>
+                            e => {
+                                this.index++;
+                                return (
+                                <tr key={e[0]}>
+                                    <td>{this.index}</td>
                                     <td>{e[0]}</td>
                                     <td>{e[1]}</td>
                                     <td>{e[2]}</td>
                                 </tr>
+                                )
+                            }
                         )
                     }
                 </tbody>
